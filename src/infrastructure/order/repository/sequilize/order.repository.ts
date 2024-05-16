9b4ce0e5b4793e749f075e38e92327abd2cd1d78
joao <joaopedroemerim@gmail.com>
2024-05-15 21:19:23 -0300

    add order repository implementation

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,4 +1,5 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
@@ -21,5 +22,77 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    await OrderModel.sequelize.transaction(async (transaction) => {
+      await OrderItemModel.destroy({
+        where: { order_id: entity.id },
+        transaction,
+      });
+
+      const items = entity.items.map((item) => {
+        return {
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+          order_id: entity.id,
+        };
+      });
+
+      await OrderItemModel.bulkCreate(items, { transaction });
+
+      await OrderModel.update(
+        { total: entity.total() },
+        { where: { id: entity.id }, transaction }
+      );
+    });
+  }
+
+  async find(id: string): Promise<Order> {
+    const order = await OrderModel.findByPk(id, {
+      include: [{ model: OrderItemModel }],
+    });
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    return new Order(
+      order.id,
+      order.customer_id,
+      order.items.map((item) => {
+        return new OrderItem(
+          item.id,
+          item.name,
+          item.price,
+          item.product_id,
+          item.quantity
+        );
+      })
+    );
+  }
+
+  async findAll(): Promise<Order[]> {
+    const orders = await OrderModel.findAll({
+      include: [{ model: OrderItemModel }],
+    });
+    return orders.map((order) => {
+      return new Order(
+        order.id,
+        order.customer_id,
+        order.items.map((item) => {
+          return new OrderItem(
+            item.id,
+            item.name,
+            item.price,
+            item.product_id,
+            item.quantity
+          );
+        })
+      );
+    });
   }
 }
